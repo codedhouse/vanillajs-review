103e27c7eea1d28c13aa1d627c28bdba26dcc01f
Vivien <vivien-public@pm.me>
2025-06-01 19:03:06 -0400

    Numbers

---
+++ numbers/script.js
@@ -0,0 +1,14 @@
+// Numbers
+
+const x = 1234;
+const xProto = x.__proto__;
+console.log(xProto);
+
+// Get number of digits
+console.log(x.toString().length);
+
+// Math methods
+console.log(Math);
+
+// Get random integer between 1-100
+console.log(Math.floor(Math.random() * 100 + 1));
