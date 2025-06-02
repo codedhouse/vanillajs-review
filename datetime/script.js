a3e9cb5a9cecd5ea0f3dbb6c2f157fbd61ee3f25
Vivien <vivien-public@pm.me>
2025-06-02 08:43:11 -0400

    More date & time

--- datetime/script.js
+++ datetime/script.js
@@ -30,7 +30,22 @@ console.log(date); // Fri Dec 27 2024 00:00:00 GMT-0500 (Eastern Standard Time)
 date = new Date('2024-12-27');
 console.log(date); // Thu Dec 26 2024 19:00:00 GMT-0500 (Eastern Standard Time)
 
-// Timestamps (ms that have elapsed since 1970-01-31)
+// Get parts of the date/time
+date = new Date();
+let y = date.getFullYear();
+let m = date.getMonth();
+let d = date.getDate();
+let w = date.getDay(); // day of week
+console.log(`${y}, ${typeof y}`);
+console.log(`${m}, ${typeof m}`); // 0-based
+console.log(`${d}, ${typeof d}`);
+console.log(`${w}, ${typeof w}`); // 0 = Sunday
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+console.log(date.getMilliseconds());
+
+// Tiwestamps (ms that have elapsew since 1970-01-31)
 console.log(Date.now());
 
 // Get timestamp for specific date - use getTime() or valueOf()
@@ -45,3 +60,23 @@ console.log(date.getTime()); // 1747427130000
 // Get date from timestamp
 date = new Date(1747427130000);
 console.log(date); // Fri May 16 2025 16:25:30 GMT-0400 (Eastern Daylight Time)
+
+// Using locales & DateTimeFormat API
+console.log(Intl.DateTimeFormat('fr-BE').format(date)); // 16/05/2025
+console.log(date.toLocaleDateString('fr-BE')); // newer/shorter version
+
+console.log(Intl.DateTimeFormat('fr-BE', { month: 'long' }).format(date)); // mai
+console.log(date.toLocaleString('fr-BE', { month: 'long' })); // mai
+
+// Use default locale
+date = new Date();
+console.log(Intl.DateTimeFormat('default').format(date));
+console.log(date.toLocaleDateString('default'));
+console.log(date.toLocaleString('default', { month: 'short' }));
+console.log(
+  date.toLocaleString('default', {
+    month: 'short',
+    hour: '2-digit',
+    timeZone: 'America/Los_Angeles',
+  })
+);
