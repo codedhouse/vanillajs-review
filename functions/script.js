6bbea6a766a339539249f4a2c1ec67bf93d19f60
Vivien <vivien-public@pm.me>
2025-06-03 11:59:14 -0400

    More functions

--- functions/script.js
+++ functions/script.js
@@ -1,10 +1,41 @@
 // Functions
 
+// Declared functions can be called before they are declared in the code
+showMessage();
+
+// Function expressions can't be called before initialization
+// const numTest = getNumDigits(3456); // error
+
 // Create
 function showMessage() {
   console.log('Hello, this is a test.');
   return; // optional, returns no value
 }
+
+const getNumDigits = function (num) {
+  const result = num.toString().length;
+  return `${num} has ${result} digits.`;
+};
+
+// Rewritten as arrow function
+const getNumDigits2 = (num) => {
+  const result = num.toString().length;
+  return `${num} has ${result} digits.`;
+};
+
+// () are optional around single parameter, e.g.
+// const getNumDigits2 = num =>
+
+// Shorter arrow function syntax when only returning a value
+const isPassingGrade = (score) => score >= 75;
+const getMax = (n1, n2) => Math.max(n1, n2);
+
+// Short arrow function that returns an object
+const getStudent = () => ({
+  firstName: 'liam',
+  lastName: 'finn',
+  age: '11',
+});
 
 const w = 100;
 console.log(w);
@@ -26,6 +57,20 @@ function addTodos(...todos) {
   return todos;
 }
 
+// Nesting
+function parent() {
+  const parentMsg = 'parent';
+  console.log(parentMsg);
+
+  function child() {
+    const childMsg = 'child';
+    console.log(childMsg, parentMsg);
+  }
+
+  child();
+  return;
+}
+
 // Call
 showMessage();
 console.log(getAvg(100, 300, 225));
@@ -34,3 +79,10 @@ addExpense(25.0, 'nintendo');
 addExpense(134.78);
 let output = addTodos('label boxes', 'vacuum bedroom', 'dust desk');
 console.log(output);
+parent();
+console.log(getNumDigits(293842845733467));
+console.log(getNumDigits2(765));
+console.log(isPassingGrade(99));
+console.log(isPassingGrade(69));
+console.log(getMax(45, 71));
+console.log(getStudent());
