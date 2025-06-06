370980d500987c5e721ceb4cdd0ca218974e8a2d
Vivien <vivien-public@pm.me>
2025-06-06 16:08:45 -0400

    More on the ternary operator & if statements

--- control-flow/script.js
+++ control-flow/script.js
@@ -1,6 +1,10 @@
 // Control Flow
 
-// Shorthand If statement syntax (optional)
+// Short If statement with no Else clause
+const isLoading = true
+isLoading && console.log('Loading results...')
+
+// Short If-Else statement
 const x = 78
 const y = 33
 
@@ -12,6 +16,24 @@ let name = ''
 !name ? console.log('Anonymous') : console.log(`Thank you, ${name}`)
 name = 'Lincoln'
 !name ? console.log('Anonymous') : console.log(`Thank you, ${name}`)
+
+// Ternary operator with no Else clause
+const isReadOnly = true
+isReadOnly ? console.log("You don't have permission to edit this file.") : null
+
+// Variable assignment using ternary operator
+const isAnon = name ? false : true
+console.log(isAnon)
+
+// Ternary operator with multiple statements
+let turnsLeft = 2
+const takeTurn =
+  turnsLeft > 0
+    ? (turnsLeft--,
+      console.log(`You took a turn. You have ${turnsLeft} turn(s) left.`),
+      'gameInProgress')
+    : (console.log("Sorry, you're out of turns."), 'endOfGame')
+console.log(takeTurn)
 
 // ?? - Nullish coalescing operator - returns right side if left side is null or undefined
 console.log(1000 ?? 'hello')
