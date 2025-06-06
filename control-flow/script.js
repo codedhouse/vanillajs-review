040fbe54ea3bd0b1a61fcceb1dbd0bec9a59f0d5
Vivien <vivien-public@pm.me>
2025-06-06 14:54:16 -0400

    Ternary operator

--- control-flow/script.js
+++ control-flow/script.js
@@ -7,6 +7,12 @@ const y = 33
 if (x >= y) console.log(`${x} is >= ${y}`), console.log('this is true')
 else console.log(`${x} is less than ${y}`)
 
+// Ternary operator
+let name = ''
+!name ? console.log('Anonymous') : console.log(`Thank you, ${name}`)
+name = 'Lincoln'
+!name ? console.log('Anonymous') : console.log(`Thank you, ${name}`)
+
 // ?? - Nullish coalescing operator - returns right side if left side is null or undefined
 console.log(1000 ?? 'hello')
 console.log(0 ?? 'hello')
