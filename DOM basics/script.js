ff6a99164987e115e9e6f8a478b518763a294a22
Vivien <vivien-public@pm.me>
2025-06-07 09:05:30 -0400

    More DOM basics

--- DOM basics/script.js
+++ DOM basics/script.js
@@ -15,9 +15,20 @@ console.dir(document.getElementById('main'))
 console.log(document.querySelector('#main ul'))
 
 // get element by using properties (old way)
+console.log(document.documentElement) // returns <html> element
 console.dir(document.body)
 console.log(document.body.innerHTML) // returns html only
 console.log(document.body.innerText) // returns text only
+console.log(document.body.children) // returns HTMLCollection of direct children
+
+// get element attributes
+console.log(document.querySelector('div').id)
+
+// get document properties
+console.log(document.characterSet)
+console.log(document.contentType)
+console.log(document.doctype)
+console.log(document.URL)
 
 // modify element html
 // document.body.innerHTML = '<h1>No Links</h1>'
@@ -27,4 +38,8 @@ const header = (document.querySelector('#main h1').innerText = 'Home')
 
 // get all links
 console.log(document.links) // returns HTMLCollection
-console.log(document.links[1])
+
+// get/modify HTMLCollection elements/attributes
+console.log(document.links[0])
+console.log(document.body.children[1])
+document.links[1].innerText = 'Reddit!'
